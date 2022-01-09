5280b1bde9e180a864e54773d33760c9903ff5a6
zbranca cristina <zbrancacristina@gmail.com>
2022-01-09 23:35:24 +0200

    Add project files.

---
+++ Zbranca_Cristina_lab10/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Nume_Pren_Lab10.Data;
+using Xamarin.Forms;
+
+namespace Zbranca_Cristina_lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    } }
---
+++ Zbranca_Cristina_lab10/ListPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zbranca_Cristina_lab10
+{
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        object Navigation = null;
+        await Navigation.PopAsync();
+    }
+}
